85d66a2f1e5138c07a8a5e3f5ead6f32d1b5a99b
Karim272 <kgaraev_3@mail.ru>
2025-03-21 11:31:25 +0300

    add spring

---
+++ src/main/java/ru/unio/UnioApplication.java
@@ -0,0 +1,13 @@
+package ru.unio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/unio/UnioApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.unio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
